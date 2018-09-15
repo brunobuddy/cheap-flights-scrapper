8f5a0f4e686a66decc1f02e62dc2ea1572cdaaa3
Bruno Pérez <bruno.perez@buddyweb.fr>
2018-09-15 13:38:04 +0200

    add express and setub node-cron

--- main.js
+++ main.js
@@ -3,55 +3,65 @@ require('dotenv').config()
 const puppeteer = require('puppeteer')
 const fs = require('fs')
 const login = require('facebook-chat-api')
-;(async () => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+const cron = require('node-cron')
+const express = require('express')
 
-    await page.goto('https://www.voyagespirates.fr/tag/erreur-de-pri')
+app = express()
 
-    // standard JS : Get first occurrence of item, in our case titles are <a> inside <h2>
-    const lastDeal = await page.$$eval('h2 > a', anchors => {
-        return anchors[0].innerText
-    })
+// Schedule CRON to execute each 5 minutes
+cron.schedule('*/5 * * * *', function() {
+    ;(async () => {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
 
-    await browser.close()
+        await page.goto('https://www.voyagespirates.fr/tag/erreur-de-pri')
 
-    // Use JSON file instead of DB (one string only to store) We use try/catch to prevent throwing error if no JSON file
-    let previousDeal
-    try {
-        const json = fs.readFileSync('previous-deal.json')
-        previousDeal = JSON.parse(json).name
-    } catch (err) {}
+        // standard JS : Get first occurrence of item, in our case titles are <a> inside <h2>
+        const lastDeal = await page.$$eval('h2 > a', anchors => {
+            return anchors[0].innerText
+        })
 
-    // We don't do anything if not updated
-    if (lastDeal === previousDeal) {
-        console.log('Nothing new')
-        return false
-    }
+        await browser.close()
 
-    // Create or update JSON
-    const data = JSON.stringify({ name: lastDeal })
-    fs.writeFileSync('previous-deal.json', data)
+        // Use JSON file instead of DB (one string only to store) We use try/catch to prevent throwing error if no JSON file
+        let previousDeal
+        try {
+            const json = fs.readFileSync('previous-deal.json')
+            previousDeal = JSON.parse(json).name
+        } catch (err) {}
 
-    // Send message through a Facebook account
-    login(
-        {
-            email: process.env.SENDER_USERNAME,
-            password: process.env.SENDER_PASSWORD
-        },
-        (err, api) => {
-            if (err) return console.error(err)
-
-            const recipients = process.env.RECIPIENTS.split(',')
-
-            recipients.forEach(recipient => {
-                var facebookId = recipient
-                var msg =
-                    lastDeal +
-                    ' ' +
-                    'https://www.voyagespirates.fr/tag/erreur-de-pri'
-                api.sendMessage(msg, facebookId)
-            })
+        // We don't do anything if not updated
+        if (lastDeal === previousDeal) {
+            console.log('Nothing new')
+            return false
         }
-    )
-})()
+
+        // Create or update JSON
+        const data = JSON.stringify({ name: lastDeal })
+        fs.writeFileSync('previous-deal.json', data)
+
+        // Send message through a Facebook account
+        login(
+            {
+                email: process.env.SENDER_USERNAME,
+                password: process.env.SENDER_PASSWORD
+            },
+            (err, api) => {
+                if (err) return console.error(err)
+
+                const recipients = process.env.RECIPIENTS.split(',')
+
+                recipients.forEach(recipient => {
+                    var facebookId = recipient
+                    var msg =
+                        lastDeal +
+                        ' ' +
+                        'https://www.voyagespirates.fr/tag/erreur-de-pri'
+                    api.sendMessage(msg, facebookId)
+                })
+            }
+        )
+    })()
+})
+
+app.listen(4444)
